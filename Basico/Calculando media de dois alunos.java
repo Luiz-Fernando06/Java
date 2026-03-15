d106608655eea86a532375d23a0c4ea4a583126a
Luiz Fernando <e.luizfernando2006@gmail.com>
2026-03-15 19:20:28 -0300

    Criar Calculando media de dois alunos.java

---
+++ Basico/Calculando media de dois alunos.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		double nota1 = 5.5;
+		double nota2 = 6.8;
+		
+		double media = (nota1 + nota2) / 2;
+		
+		System.out.println("Primeira nota: " + nota1);
+		System.out.println("Segunda nota: " + nota2);
+		
+		System.out.println("Media do aluno: " + media);
+	}
+	
+}
