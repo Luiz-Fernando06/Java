8c27e321c717fd3dc5403c56bcb0579d225790a4
Luiz Fernando <e.luizfernando2006@gmail.com>
2026-03-15 19:23:01 -0300

    Criar AscII.java

---
+++ Basico/AscII.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		/* 6. Operações com char
+	Crie uma variável char com uma letra.
+	Mostre:
+	a letra
+	o código ASCII dela convertendo para int.*/
+	
+	char letra = 'A';
+	char letra2 = 65;
+	System.out.println((int)letra);
+	System.out.println(letra2);
+	}
+}
